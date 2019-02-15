ea69dee1c10710ad4d54c74d01304d215f93d5cf
Jayeshgiri.Bavaji <jayeshgiri.bavaji@synechron.com>
2019-02-15 19:39:33 +0530

    Commit 5 - Kotlin Intent (Explicit / Implicit)

--- app/src/main/java/com/jbavaji/messageshareapp/MainActivity.kt
+++ app/src/main/java/com/jbavaji/messageshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jbavaji.messageshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,8 +19,22 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, resources.getString(R.string.button_click), Toast.LENGTH_SHORT).show()
         }
 
-        btnShowToastAgain.setOnClickListener {
-            Toast.makeText(this, resources.getString(R.string.button_click_again), Toast.LENGTH_SHORT).show()
+        btnSendToActivity.setOnClickListener {
+            val message : String = etUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+
+        btnShareToOtherActivity.setOnClickListener {
+            val message : String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Please Select Share App - "))
         }
     }
 }
